da13e6e48e580fbb9e2f60560d008af66578249a
thxssio <thxssio@gmail.com>
2024-05-11 16:51:22 -0300

    Created React App

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
+import { logo } from "../../assets/index";
+
+const Footer = () => {
+  return (
+    <div className="w-full py-20 h-auto border-b-[1px] border-b-black grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
+      <div className="w-full h-full flex flex-col gap-8">
+        <img className="w-32" src={logo} alt="logo" />
+        <div className="flex gap-4">
+          <span className="bannerIcon">
+            <FaFacebookF />
+          </span>
+          <span className="bannerIcon">
+            <FaTwitter />
+          </span>
+          <span className="bannerIcon">
+            <FaLinkedinIn />
+          </span>
+        </div>
+      </div>
+      <div className="w-full h-full">
+        <h3 className="text-xl uppercase text-designColor tracking-wider">
+          Endereço
+        </h3>
+        <p className="text-lg font-titleFont font-medium py-6">
+          123 Rua Exemplo, Bairro Fictício, Cidade Imaginária, 01234-567
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
